edba0f8ef1efe29256098952edb9ffc3a0e253db
anther1989 <anther1989@hotmail.com>
2025-05-15 00:05:26 -0600

    Commit

--- Sources/NotificationsServiceExtension/NotificationService.swift
+++ Sources/NotificationsServiceExtension/NotificationService.swift
@@ -14,24 +14,25 @@ class NotificationService: UNNotificationServiceExtension {
 	var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        print("Entró a didReceive en NotificationServiceExtension")
         self.receivedRequest = request;
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
         //Handle the Engage Push response//
-        if Engage.shared.isEngagePayload(bestAttemptContent.userInfo){
+        /*if Engage.shared.isEngagePayload(bestAttemptContent?.userInfo){
             Engage.shared.handleNSEPush(
                 request,
                 using: "group.com.firstorion.enterprise.engagecalling.uae", // Insert your App Group ID
                 includeThumbnail: false, // (Optional) Defaults to true if left out
                 withContentHandler: contentHandler // Pass in the content handler parameter
             )
-        } else {
+        } else {*/
             //Handle other Push Response like OneSignal //
             if let bestAttemptContent = bestAttemptContent {
                 OneSignalExtension.didReceiveNotificationExtensionRequest(self.receivedRequest, with: bestAttemptContent, withContentHandler: contentHandler)
             }
-        }
+        //}
     }
 
 	override func serviceExtensionTimeWillExpire() {

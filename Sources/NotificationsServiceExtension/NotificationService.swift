6a7f00b908153735010939bbf5eb13be162ddb54
anther1989 <anther1989@hotmail.com>
2025-05-14 22:59:11 -0600

    Se agrega engage a extension service

--- Sources/NotificationsServiceExtension/NotificationService.swift
+++ Sources/NotificationsServiceExtension/NotificationService.swift
@@ -5,6 +5,7 @@
 
 import UserNotifications
 import OneSignalExtension
+import EngageKit
 
 class NotificationService: UNNotificationServiceExtension {
 
@@ -12,15 +13,26 @@ class NotificationService: UNNotificationServiceExtension {
 	var receivedRequest: UNNotificationRequest!
 	var bestAttemptContent: UNMutableNotificationContent?
 
-	override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-		self.receivedRequest = request;
-		self.contentHandler = contentHandler
-		bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-
-		if let bestAttemptContent = bestAttemptContent {
-			OneSignalExtension.didReceiveNotificationExtensionRequest(self.receivedRequest, with: bestAttemptContent, withContentHandler: contentHandler)
-		}
-	}
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.receivedRequest = request;
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        //Handle the Engage Push response//
+        if Engage.shared.isEngagePayload(bestAttemptContent.userInfo){
+            Engage.shared.handleNSEPush(
+                request,
+                using: "group.com.firstorion.enterprise.engagecalling.uae", // Insert your App Group ID
+                includeThumbnail: false, // (Optional) Defaults to true if left out
+                withContentHandler: contentHandler // Pass in the content handler parameter
+            )
+        } else {
+            //Handle other Push Response like OneSignal //
+            if let bestAttemptContent = bestAttemptContent {
+                OneSignalExtension.didReceiveNotificationExtensionRequest(self.receivedRequest, with: bestAttemptContent, withContentHandler: contentHandler)
+            }
+        }
+    }
 
 	override func serviceExtensionTimeWillExpire() {
 		// Called just before the extension will be terminated by the system.
